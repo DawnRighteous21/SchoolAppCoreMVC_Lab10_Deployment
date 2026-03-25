956aed996d3d863c273df6d891e2279ad71f068c
David Duy <davidduy06@gmail.com>
2026-03-25 19:51:22 +0700

    Add project files.

---
+++ SchoolAppCoreMVC/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolAppCoreMVC.Models;
+
+public class Department
+{
+    public int DepartmentID { get; set; }
+    public string Name { get; set; }
+    public decimal Budget { get; set; }
+    public DateTime StartDate { get; set; }
+}
---
+++ SchoolAppCoreMVC/Models/Instructor.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolAppCoreMVC.Models;
+
+public class Instructor
+{
+    public int InstructorID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime HireDate { get; set; }
+}
